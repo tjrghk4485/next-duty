96a08d6e1c84f22fba15f2ae39dea53da3cd86fd
tjrghk4485 <124678859+tjrghk4485@users.noreply.github.com>
2025-09-05 13:37:32 +0900

    fix: vercel빌드에러처리

--- src/app/post-detail/page.js
+++ src/app/post-detail/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense} from "react";
 import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
 import Link from "next/link";
